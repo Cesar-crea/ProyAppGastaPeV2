98b7ba5460dd5e7b110bf16e3c109e0bc514f147
Ranhaurg82 <ornning82@gmail.com>
2026-06-02 00:18:29 -0500

    vistas realizadas de login y registro(Josue)

--- app/src/main/java/com/example/proyectappgastape/activities/SplashActivity.kt
+++ app/src/main/java/com/example/proyectappgastape/activities/SplashActivity.kt
@@ -1,17 +1,40 @@
 package com.example.proyappgastape.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.proyappgastape.R
+//import com.example.proyappgastape.R
+import com.example.proyectappgastape.R
+import com.example.proyectappgastape.activities.DashboardActivity
+import com.example.proyectappgastape.activities.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private var timer : CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+        timer = object : CountDownTimer(2000, 1000){
+            override fun onFinish() {
+                val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                // codigo a ejecutarse por cada segundo
+            }
+
+        }.start()
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
